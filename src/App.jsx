491564260cf7a5a12255cbfe8beecc5457de5c25
lmossman <lake@airbyte.io>
2023-06-15 17:05:39 -0700

    example of broken rendering

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React, { Suspense } from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { ReflexContainer, ReflexSplitter, ReflexElement } from 'react-reflex';
+import "./App.css"
+import 'react-reflex/styles.css'
+
+// Create a client
+const queryClient = new QueryClient();
+
+// Create a mock fetch function with delay
+const fetchMockData = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  const data = await res.json();
+  // To simulate network delay
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  return data;
+}
+
+const QueryComponent = () => {
+  const { data } = useQuery('fetchMockData', fetchMockData, { suspense: true });
+  console.log(data);
+  return null
+}
+
+const ReflexComponent = () => {
+  console.log("Rendering ReflexComponent")
+  return (
+    <div className="container">
+    <ReflexContainer orientation="vertical">
+      <ReflexElement className="left-pane"> 
+        <div className="pane-content">
+          <label>
+            Left Pane
+          </label>
+        </div>
+      </ReflexElement>
+      <ReflexSplitter/>
+      <ReflexElement className="right-pane">
+        <div className="pane-content">
+          <label>
+            Right Pane
+          </label>
+        </div>
+      </ReflexElement>
+    </ReflexContainer>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<div>Loading...</div>}> 
+        <QueryComponent />
+        <ReflexComponent />
+      </Suspense>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
